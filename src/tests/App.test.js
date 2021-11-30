f9149a3691a51d851c97747e0ba7477a4cfaa1b6
Emerson <eemr3@yahoo.com.br>
2021-11-30 16:04:47 -0300

    test(App): implementa testes do App.js requisito - 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+describe('Teste do componete App', () => {
+  it('Testa renderização do topo da aplicação e links', () => {
+    renderWithRouter(<App />);
+    const titleHeaderApp = screen.getByText(/pokédex/i);
+    expect(titleHeaderApp).toBeInTheDocument();
+  });
+
+  it('testa se link home esta renderizado na página e se é redirecionada para rota \'/\'',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const linkHome = screen.queryByRole('link', { name: /home/i });
+      expect(linkHome).toBeDefined();
+      userEvent.click(linkHome);
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+    });
+
+  it('testa se link about renderizado na página', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.queryByRole('link', { name: /about/i });
+    expect(linkAbout).toBeDefined();
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('testa se link Favorite Pokémons renderizado na página', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavoritePokemons = screen.queryByRole('link', {
+      name: /favorite pokémons/i });
+    expect(linkFavoritePokemons).toBeDefined();
+    userEvent.click(linkFavoritePokemons);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('testa se link inexistente leva a página Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const pageNotfound = screen.queryByRole('heading',
+      { name: /Page requested not found 😭/i });
+    expect(pageNotfound).toBeDefined();
+  });
+});

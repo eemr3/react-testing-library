bd910f0e2bf4a608fd7814892e0b5ce48df4e84d
Emerson <eemr3@yahoo.com.br>
2021-11-30 17:52:11 -0300

    refactor=>test(About): implementa testes do About.js requisito - 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -10,16 +10,25 @@ describe('Teste o componente <About.js />', () => {
     renderWithRouter(<About />);
     const contentPageAbout = screen.getAllByText(/p/);
     expect(contentPageAbout[0]).toHaveTextContent(paragraphOne);
+  });
 
-    const imagePokedex = screen.getByRole('img', { name: 'Pokédex' });
-    expect(imagePokedex).toBeInTheDocument();
+  it('Teste se a página contém um heading h2 com o texto \'About Pokédex\'', () => {
+    renderWithRouter(<About />);
 
     const titleAbout = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
     expect(titleAbout).toBeInTheDocument();
+  });
 
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
     const paragraphs = screen.getAllByText(/p/);
     expect(paragraphs).toHaveLength(2);
+  });
 
+  it('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+    const imagePokedex = screen.getByRole('img', { name: 'Pokédex' });
+    expect(imagePokedex).toBeInTheDocument();
     expect(imagePokedex).toHaveAttribute('src',
       'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
